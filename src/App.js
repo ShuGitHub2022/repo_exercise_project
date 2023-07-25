7338def742bf39d66ed49befe2f482ab278f2cf9
ShuGitHub2022 <liuchen7978@hotmail.com>
2023-07-24 23:10:55 -0700

    add meta data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 
 import './App.css';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
-function App() {
+<>
+<meta name="og:title" content="Little Lemon Restaurant" />
+<meta name="description" content="Little Lemon Restaurant provides you delicious food and satisfy your tast buds"/>
+<meta name="og:description" content="Little Lemon Restaurant located in Chicago"/>
+</>
+
+  function App () {
   return (
-    <header>
-      
-    </header>
-    <nav>
+    <>
+    <Header />
+    <Nav />
+    <Main />
+    <Footer />
+    </>
 
-    </nav>
-    <main>
-
-    </main>
-    <footer>
-      
-    </footer>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Nav from "./Nav";
+import { Box, HStack } from "@chakra-ui/react";
 
 const Header = () => {
     return (
+        <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        ></HStack>
+            <img src="./images/littlelemonlogo.png" alt="LittleLemonLogo" width="42" height="50"/>
+            <meta name="og:image" content="little lemon logo"/>
+            <Nav/>  
+        </Box>
         
     );
 
